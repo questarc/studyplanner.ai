f80494c6e089f84e8bba7b9dbc8602f1de76bae4
questarc <questarc.tech@gmail.com>
2025-10-05 06:17:12 -0400

    Update planner_agent.py

--- planner_agent.py
+++ planner_agent.py
@@ -1,12 +1,17 @@
 from langgraph.graph import StateGraph
 from langchain_core.runnables import RunnableLambda
+from typing import TypedDict, List, Dict
 
-def collect_subjects(state):
-    subjects = state["subjects"]
-    exams = state["exams"]
-    return {"subjects": subjects, "exams": exams}
+class PlannerState(TypedDict):
+    subjects: List[str]
+    exams: Dict[str, int]
+    plan: Dict[str, List[str]]
+    quote: str
 
-def generate_plan(state):
+def collect_subjects(state: PlannerState) -> PlannerState:
+    return {"subjects": state["subjects"], "exams": state["exams"]}
+
+def generate_plan(state: PlannerState) -> PlannerState:
     subjects = state["subjects"]
     exams = state["exams"]
     plan = {}
@@ -15,11 +20,11 @@ def generate_plan(state):
         plan[subject] = [f"Day {i+1}: Study {subject}" for i in range(days_left)]
     return {"plan": plan}
 
-def motivate(state):
+def motivate(state: PlannerState) -> PlannerState:
     from utils.quotes import get_motivational_quote
     return {"quote": get_motivational_quote()}
 
-graph = StateGraph()
+graph = StateGraph(PlannerState)
 graph.add_node("collect", RunnableLambda(collect_subjects))
 graph.add_node("plan", RunnableLambda(generate_plan))
 graph.add_node("motivate", RunnableLambda(motivate))
